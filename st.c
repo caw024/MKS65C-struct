62b7dfc12782e5a8f81421657885f109415b23ff
hasifahmed <hahmed@stuy.edu>
2018-10-02 15:35:52 -0400

    pokemons

--- st.c
+++ st.c
@@ -2,7 +2,24 @@
 
 struct pokemon {char * type;int level;};
 
+struct pokemon Pikachu;
+Pikachu.type = "Electric";
+Pikachu.level = 20;
 
+struct pokemon Charizard;
+Pikachu.type = "Fire";
+Pikachu.level = 5;
+
+struct pokemon Blastoise;
+Pikachu.type = "Aqua";
+Pikachu.level = 500;
+
+struct pokemon pokemons[] = {Pikachu, Charizard, Blastoise};
+
+struct pokemon info(){
+  
+  
+}
 
 
 

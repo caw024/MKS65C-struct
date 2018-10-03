37de5b7ed637925415a93f3e09840f6a203b3ec0
Calvin Aw <caw@stuy.edu>
2018-10-03 18:04:26 -0400

    yayer

--- st.c
+++ st.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-struct pokemon {char name[15];int level;};
+#include <string.h>
 
-struct pokemon Pikachu;
-strcpy(Pikachu.name,"Pikachu");
-Pikachu.level = 20;
+struct pokemon {char name[20];int level;};
+struct pokemon pokemons[3];
 
-struct pokemon Charizard;
-strcpy(Charizard.name,"Charizard");
-Charizard.level = 5;
-
-struct pokemon Blastoise;
-strcpy(Blastoise.name,"Blastoise");
-Blastoise.level = 500;
-
-struct pokemon pokemons[4] = { Pikachu,Charizard, Blastoise};
 
 struct pokemon info(){
   srand(time(NULL));
   return pokemons[rand() % 3];
-  
-  
 }
 
 int main(){
+
+  struct pokemon Pikachu;
+  strcpy(Pikachu.name,"Pikachu");
+  Pikachu.level = 20;
+
+  struct pokemon Charizard;
+  strcpy(Charizard.name,"Charizard");
+  Charizard.level = 5;
+
+  struct pokemon Blastoise;
+  strcpy(Blastoise.name,"Blastoise");
+  Blastoise.level = 500;
+
+  pokemons[0] = Pikachu;
+  pokemons[1] = Charizard;
+  pokemons[2] = Blastoise;
+  
   struct pokemon rando = info();
-  printf("Pokemon: %s/nLevel: %d",rando.name,rando.level);
+  
+  printf("Pokemon: %s\nLevel: %d\n",rando.name,rando.level);
 }
